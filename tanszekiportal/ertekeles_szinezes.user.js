5c6262a1ad47759c3d3dc2f02d538b2a96bb7a3c
madve2 <madve2@gmail.com>
2016-11-30 17:11:35 +0100

    Az ertekeles_szinezes letiltja a Ment gombot, ha több sor is változott
    ...mivel a Ment gomb hatására a többi módosítás hajlamos elveszni. Helyette tooltipben promotálja az Összes eredmény mentése gombot.

--- tanszekiportal/ertekeles_szinezes.user.js
+++ tanszekiportal/ertekeles_szinezes.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Tanszéki portál értékelések színezése
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  A tanszéki portálon az értékelések listájánál a nem értékelt sorok hátterét kiszínezi
-// @author       Ákos Nagy
+// @author       Ákos Nagy, Dániel Kis-Nagy
 // @match        https://www.aut.bme.hu/Course/*/*/*/*
 // @grant        none
 // ==/UserScript==
@@ -13,7 +13,7 @@
 var rowSelector = '#gvUserOnAssignmentResult > tbody > tr.gridViewRow';
 var rowAltSelector = '#gvUserOnAssignmentResult > tbody > tr.gridViewAltRow';
 var rows=$(rowSelector + ', ' + rowAltSelector);
-
+var changedCount = 0;
 
 rows.each(function() {
     var grade = $(this).find('option:selected');
@@ -25,4 +25,14 @@ rows.each(function() {
 
 $(rowSelector + ' select, ' + rowAltSelector + ' select').change(function(e) {
     $(e.currentTarget.parentNode.parentNode).css('background-color','#FFCE43');
+    changedCount++;
+    // Ha több változásunk van, akkor a Ment gombot letiltjuk, ugyanis azzal
+    // nagyon könnyen el tudjuk veszíteni az összes többi módosításunkat...
+    if (changedCount > 1) {
+        $(".btnAccept[value='Ment']").each(function() {
+            this.disabled = true;
+            this.style.opacity = "0.5";
+            this.title = "Több sor is megváltozott. Használd inkább az Összes eredmény mentése gombot!";
+        });
+    }
 });
